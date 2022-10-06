833ae3126042a4b1f96b9c8b3d8209d17497dd3b
Nils Rothamel <nils@Nilss-Air.lan>
2022-10-06 17:21:54 +0100

    Commit after exercise 14 (Abstract class)

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,19 +1,13 @@
 import com.googlecode.lanterna.SGR;
 import com.googlecode.lanterna.TerminalPosition;
-import com.googlecode.lanterna.TextCharacter;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
-import com.googlecode.lanterna.screen.Screen;
 
-public class Hero {
-    private Position position;
+public class Hero extends Element{
 
     public Hero(int x, int y) {
-        position = new Position(x,y);
+        super(x, y);
     }
-   public void setPosition(Position position){
-        this.position = position;
-   }
     public Position moveUp(){
         return new Position(position.getX(), position.getY() - 1);
     }
